b3b04a6c9cbab6d164d02e5ce8f7fe8d98375169
cwern40 <chris.wernli@hotmail.com>
2019-08-17 11:08:04 -0600

    A created validateProjectId middleware to check if the project Id is valid

--- data/helpers/ProjectRouter.js
+++ data/helpers/ProjectRouter.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
         })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateProjectId, (req, res) => {
     const { id } = req.params
 
     Projects.get(id)
@@ -31,5 +31,27 @@ router.get('/:id', (req, res) => {
             })
         })
 })
+
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+
+    Projects.get(id)
+        .then(project => {
+            if (project) {
+                req.project = project
+                next();
+            } else {
+                res.status(404).json({
+                    message: 'Invalid project id'
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'error processing the request'
+            })
+        })
+}
 
 module.exports = router;
