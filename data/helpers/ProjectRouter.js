21c51b534544c4011aeff014e8538049344ff4f7
cwern40 <chris.wernli@hotmail.com>
2019-08-17 10:56:59 -0600

    I created the get request for projects

--- data/helpers/ProjectRouter.js
+++ data/helpers/ProjectRouter.js
@@ -4,4 +4,17 @@ const router = express.Router();
 
 const Projects = require('./projectModel')
 
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: 'error retrieving the list of projects'
+            })
+        })
+})
+
 module.exports = router;
