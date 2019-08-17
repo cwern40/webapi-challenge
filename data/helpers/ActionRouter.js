701b68afffb03be7696520342d4c232a9cd6ee29
cwern40 <chris.wernli@hotmail.com>
2019-08-17 11:30:14 -0600

    I created the get request for the actions

--- data/helpers/ActionRouter.js
+++ data/helpers/ActionRouter.js
@@ -4,4 +4,30 @@ const router = express.Router();
 
 const Actions = require('./actionModel')
 
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: 'error retrieving actions'
+            })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    Actions.get(req.params.id)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: 'error retrieving action'
+            })
+        })
+})
+
 module.exports = router;
